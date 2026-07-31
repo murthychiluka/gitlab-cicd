defb71469c2ad6fca3cb3a2914c70ced8fe4c1d0
murthychiluka <murthychiluka@gmail.com>
2026-07-31 11:37:45 +0530

    added provider block

--- main.tf
+++ main.tf
@@ -1,3 +1,9 @@
+
+provider "aws" {
+    region = "us-east-1"
+    
+}
+
 resource "aws_instance" "web" {
   ami           = "ami-0cca150d127c2216f"  # Example Amazon Linux 2 AMI
   instance_type = lookup(var.instance_type, "dev", "t2.nano")
